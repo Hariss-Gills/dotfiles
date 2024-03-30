25ddff66d55cb3f1d48989a3974b5021d383690f
Hariss Ali Gills <harissgills@gmail.com>
2024-03-30 02:39:23 +0000

    neovim config setup

---
+++ .config/nvim/lua/chadrc.lua
@@ -0,0 +1,11 @@
+-- This file  needs to have same structure as nvconfig.lua 
+-- https://giithub.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "catppuccin",
+}
+
+return M
