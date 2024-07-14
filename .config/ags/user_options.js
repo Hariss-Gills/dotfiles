d86e12917533dd7c1e086de63554bd4dc5fc7efb
Hariss Ali Gills <harissgills@gmail.com>
2024-07-14 15:31:27 +0300

    add some dotfiles based on https://end-4.github.io/dots-hyprland-wiki/en/

---
+++ .config/ags/user_options.js
@@ -0,0 +1,17 @@
+// For every option, see ~/.config/ags/modules/.configuration/user_options.js
+// (vscode users ctrl+click this: file://./modules/.configuration/user_options.js)
+// (vim users: `:vsp` to split window, move cursor to this path, press `gf`. `Ctrl-w` twice to switch between)
+//   options listed in this file will override the default ones in the above file
+
+const userConfigOptions = {
+  'sidebar': {
+    'pages': {
+        'order': ["apis", "tools"],
+        'apis': {
+            'order': ["gemini", "gpt"],
+        }
+    },
+}
+};
+
+export default userConfigOptions;
